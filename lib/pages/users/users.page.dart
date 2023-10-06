b9d9700ee6aa0bdb22b6403f8b599f85b4ee3452
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2023-10-06 22:30:13 +0300

    mise en dynamique du pagination

--- lib/pages/users/users.page.dart
+++ lib/pages/users/users.page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -9,14 +11,18 @@ class UsersPage extends StatefulWidget {
 class _UsersPageState extends State<UsersPage> {
   String? _query;
   bool _isvisible= false;
-
+  dynamic? data;
+  int currentPage =0;
+  int totalPage = 0;
+  int pageSize = 20;
+  ScrollController scrollController = ScrollController();
 
   TextEditingController queryTextEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title:  Text('User $_query'),),
+      appBar: AppBar(title:  Text('User $_query => $currentPage / $totalPage'),),
       body:  Center(
         child: Column(
           children: [
@@ -26,10 +32,11 @@ class _UsersPageState extends State<UsersPage> {
                     child: Container(
                         padding: const EdgeInsets.all(10),
                         child: TextFormField(
-                          obscureText: this._isvisible,
+                          obscureText: _isvisible,
                           onChanged: (value){
                             setState(() {
-                              this._query = value;
+                              _query = value;
+                              // _search(_query);
                             });
                           },
                           controller: queryTextEditingController,
@@ -37,10 +44,10 @@ class _UsersPageState extends State<UsersPage> {
                             // icon: const Icon(Icons.person),
                               suffixIcon: IconButton(onPressed: () {
                                 setState(() {
-                                  this._isvisible = !this._isvisible;
+                                  _isvisible = !_isvisible;
                                 });
 
-                              }, icon:   Icon( this._isvisible ? Icons.visibility_off : Icons.visibility ),),
+                              }, icon:   Icon( _isvisible ? Icons.visibility_off : Icons.visibility ),),
                               contentPadding: const EdgeInsets.only(left: 20),
                               border: OutlineInputBorder(
                                   borderSide: const BorderSide(
@@ -52,25 +59,69 @@ class _UsersPageState extends State<UsersPage> {
                           ),
                         )),
                   ),
-                  IconButton(onPressed: (){
+                  IconButton(onPressed: () async {
                     // this._query = queryTextEditingController.text;
+                    //   await _search(_query);
                     setState(() {
-                      this._query = queryTextEditingController.text;
+                      print(queryTextEditingController.text);
+                      _query = queryTextEditingController.text;
+                      _search(_query);
                     });
-                    _search(this._query);
                   }, icon: const Icon(Icons.search,color: Colors.indigo,))
                 ],
-              )
+              ),
+            Expanded(
+              child: ListView.builder( itemCount: data == null ? 0 : data['items'].length,itemBuilder: (context,index){
+                return ListTile(
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            backgroundImage: NetworkImage( data['items'][index]['avatar_url'] ),
+                          ),
+                          const SizedBox(width: 15),
+                          Text("${data['items'][index]['login']} "),
+
+                        ],
+                      ),
+                      CircleAvatar(
+                        radius: 15,
+                        child: Text('${data['items'][index]['score']}'),
+                      )
+                    ],
+                  ),
+                );
+              }),
+            )
           ],
         )
       ),
     );
   }
-
+  //
   Future<void> _search(String? query) async {
-    Uri url = Uri.https('api.github.com', '/search/users', {'q': query, 'per_page': '20', 'page': '0'});
+    Uri url = Uri.https('api.github.com', '/search/users', {'q': query, 'per_page': '$pageSize', 'page': '$currentPage'});
     print(url);
    var response = await http.get(url);
    print(response.body);
+   setState(() {
+     data =json.decode(response.body);
+     if(data['total_count'] % pageSize == 0 ){
+      totalPage = data['total_count'] ~/ pageSize;
+     }else{
+       totalPage = (data['total_count'] / pageSize).floor() + 1;
+     }
+   });
   }
+// void _search (String? query){
+//     Uri url = Uri.https('api.github.com', '/search/users', {'q': query, 'per_page': '20', 'page': '0'});
+//     print(url);
+//     http.get(url)
+//         .then((value) => data = jsonDecode(value.body))
+//         .catchError((err){
+//           print(err);
+//   });
+// }
 }
